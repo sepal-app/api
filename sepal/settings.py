42d833e663a0e2233c3e623cc4aaddda927bba73
Brett Adams <brettadams@fastmail.com>
2020-12-14 09:21:01 -0500

    fix pytest

--- sepal/settings.py
+++ sepal/settings.py
@@ -4,7 +4,6 @@ from pydantic import BaseSettings, PostgresDsn
 class Settings(BaseSettings):
     app_base_url: str
     database_url: PostgresDsn
-    access_token_expires_minutes = 30
     secret_key: str
 
     firebase_project_id: str

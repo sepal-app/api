c3ebc58f53b0857ffa8057a8da91f926beb567a2
Brett Adams <brettadams@fastmail.com>
2020-12-26 18:36:30 -0500

    cleanup environment variables

--- sepal/settings.py
+++ sepal/settings.py
@@ -1,13 +1,13 @@
+from typing import Optional
 from pydantic import BaseSettings, PostgresDsn
 
 
 class Settings(BaseSettings):
     app_base_url: str
     database_url: PostgresDsn
-    secret_key: str
 
     firebase_project_id: str
-    google_application_credentials_json: str
+    google_application_credentials_json: Optional[str] = None
 
     mailgun_api_key: str
     mailgun_api_url: str
